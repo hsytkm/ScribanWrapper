83cb29420e9fe30a15764a0957d4991a1e863a9b
thinva <34601860+hsytkm@users.noreply.github.com>
2024-03-23 19:59:55 +0900

    プロジェクト ファイルを追加します。

---
+++ README.md
@@ -0,0 +1,10 @@
+# ScribanWrapper
+
+This is a single file exe that wraps the [Scriban](https://github.com/scriban/scriban) template engine.
+
+When you provide the source text as an argument, it outputs the parsed text content.
+
+```sh
+$ SourceGen.exe [ScriptFile] > out.txt"
+```
+
---
+++ ScribanWrapper/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        AppDomain.CurrentDomain.AssemblyResolve += static (_, args) =>
+        {
+            AssemblyName assemblyName = new(args.Name);
+            string path = assemblyName.Name + ".dll";
+            using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            if (stream is null)
+                return null;
+
+            stream.Position = 0;
+            var assemblyRawBytes = new byte[stream.Length];
+            stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+            return Assembly.Load(assemblyRawBytes);
+        };
+
+        if (args.Length < 1)
+        {
+            Console.WriteLine("""
+                Usage: SourceGen.exe [ScriptFile] > out.txt"
+                For details on the text file format, please refer to: https://github.com/scriban/scriban
+                """);
+            return;
+        }
+
+        string scriptFile = args[0];
+        if (!File.Exists(scriptFile))
+        {
+            Console.WriteLine($"\"{scriptFile}\" not found.");
+            return;
+        }
+
+        string result = GetRenderedText(scriptFile);
+        Console.WriteLine(result);
+    }
+
+    // If you don't extract method that uses external library, a DLL error will occur.
+    private static string GetRenderedText(string scriptFile)
+    {
+        string sourceScript = File.ReadAllText(scriptFile);
+        return Scriban.Template.Parse(sourceScript).Render();
+    }
+}
